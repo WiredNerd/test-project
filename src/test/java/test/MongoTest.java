66a7de2e40b5719712bed555bdcbb7606e4ce4b5
WiredNerd <pbuschmail-gitlab@yahoo.com>
2022-02-09 11:37:30 -0600

    springboottest

---
+++ src/test/java/test/MongoTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class MongoTest {
+
+    @Test
+    void canRun(){
+        assertTrue(true);
+    }
+}
