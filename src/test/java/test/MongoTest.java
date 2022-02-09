2a7beffeeda3c6a89b1a7748b79afd51d71b18a5
WiredNerd <pbuschmail-gitlab@yahoo.com>
2022-02-09 11:42:18 -0600

    test container

--- src/test/java/test/MongoTest.java
+++ src/test/java/test/MongoTest.java
@@ -2,11 +2,18 @@ package test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@Testcontainers
 public class MongoTest {
+
+    @Container
+    private static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:" + System.getProperty("mongo", "5.0"));
 
     @Test
     void canRun(){
