053b85fe8f34ab81f3933b3ac48d10184fc02636
WiredNerd <pbuschmail-gitlab@yahoo.com>
2022-02-09 12:10:07 -0600

    output image name

--- src/test/java/test/MongoTest.java
+++ src/test/java/test/MongoTest.java
@@ -1,22 +1,42 @@
 package test;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+@DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
 @Testcontainers
 public class MongoTest {
 
     @Container
     private static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:" + System.getProperty("mongo", "5.0"));
 
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+
+    @Autowired
+    protected MongoTemplate mongoTemplate;
+
+    @Autowired
+    protected MongoDatabaseFactory mongoDatabaseFactory;
+
     @Test
     void canRun(){
+        System.out.println(mongoDBContainer.getContainerInfo().getConfig().getImage());
+
         assertTrue(true);
     }
 }
